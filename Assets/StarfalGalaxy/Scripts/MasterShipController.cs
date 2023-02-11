237197430b495eec9838ac061ae12713ccd0f290
DirtyBoots <reallydirtyboots@gmail.com>
2023-02-11 01:12:06 -0700

    Updated Master Ship Controls
    Fixed the orbit functionality, just need to fix the issue with the camera

--- Assets/StarfalGalaxy/Scripts/MasterShipController.cs
+++ Assets/StarfalGalaxy/Scripts/MasterShipController.cs
@@ -5,6 +5,7 @@ public class MasterShipController : MonoBehaviour
     public float forwardSpeed = 10f; // speed of forward movement
     public float horizontalSpeed = 5f; // speed of strafing movement
     public float mouseSensitivity = 10f; // sensitivity of mouse rotation
+    public float orbitSpeed = 2f; // speed of orbiting movement
 
     private Rigidbody rigidBody;
 
@@ -42,9 +43,16 @@ public class MasterShipController : MonoBehaviour
         float yRotation = transform.eulerAngles.y + mouseX * mouseSensitivity;
         transform.eulerAngles = new Vector3(0, yRotation, 0);
 
+        // calculate the direction to the mouse position in world space
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.z - Camera.main.transform.position.z));
+        Vector3 directionToMouse = mousePosition - transform.position;
+
         // calculate the final direction
-        vertical *= forwardSpeed;
-        Vector3 direction = new Vector3(-vertical, 0, horizontal * horizontalSpeed);
+        Vector3 forwardDirection = -transform.right * forwardSpeed * vertical;
+        Vector3 orbitDirection = Quaternion.AngleAxis(90, transform.up) * directionToMouse.normalized * orbitSpeed * horizontal;
+        Vector3 strafeDirection = transform.forward * horizontalSpeed * horizontal;
+        Vector3 direction = forwardDirection + orbitDirection + strafeDirection;
 
         // set the velocity of the rigidbody
         rigidBody.velocity = direction;
